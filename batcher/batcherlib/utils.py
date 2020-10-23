57744035b549c9f1d2714bc68e8db872d42c4bd0
Tero Vaalavuo <tero.vaalavuo@gmail.com>
2020-10-23 19:45:11 +0300

    Initial commit

---
+++ batcher/batcherlib/utils.py
@@ -0,0 +1,4 @@
+def addnumbers(a, b):
+
+    return a + b
+
---
+++ batcher/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="batcher", 
+    version="0.0.1",
+    author="Tero Vaalavuo",
+    author_email="tero.vaalavuo@gmail.com",
+    description="A utility tool to pack arrays of records to batches",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vuosoft/utils",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
